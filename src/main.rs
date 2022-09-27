2b49b076d676add9d2487af3824a3a1a4958ea07
Domenico De Guglielmo <domenico.deguglielmo@domo-iot.com>
2022-09-27 15:59:50 +0200

    rumqttc example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use rumqttc::{MqttOptions, AsyncClient, QoS};
+use std::time::Duration;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+
+    let mut mqttoptions = MqttOptions::new("rumqtt-async", "test.mosquitto.org", 1883);
+    mqttoptions.set_keep_alive(Duration::from_secs(5));
+
+    let (client, mut eventloop) = AsyncClient::new(mqttoptions, 10);
+    client.subscribe("hello/rumqtt", QoS::AtMostOnce).await.unwrap();
+
+    tokio::task::spawn(async move {
+        for i in 0..10 {
+            client.publish("hello/rumqtt", QoS::AtLeastOnce, false, vec![i; i as usize]).await.unwrap();
+            tokio::time::sleep(Duration::from_millis(100)).await;
+        }
+    });
+
+    loop {
+        let notification = eventloop.poll().await.unwrap();
+        println!("Received = {:?}", notification);
+    }
 }
